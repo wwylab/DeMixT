609811fc421c9f2e6f5397912da64ebdd13054b4
Gao <fgao3@BCBMC02X50FGJHD.local>
2019-03-31 04:06:04 -0500

    Bioconductor Check

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 #zzz.R
 
 .onAttach <- function(...) {
-  result <- .C("checkopenmp", numthread=as.integer(0))
-  packageStartupMessage("OpenMP installed.\nNumber of threads = ", result$numthread)
+    result <- .C("checkopenmp", numthread=as.integer(0))
+    packageStartupMessage("OpenMP installed.\nNumber of threads = ", 
+        result$numthread)
 }
